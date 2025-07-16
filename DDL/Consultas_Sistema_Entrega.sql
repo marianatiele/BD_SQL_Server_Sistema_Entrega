962369a49935402427521d9739d3c7004926b0ae
Maria Natiele <106673906+marianatiele@users.noreply.github.com>
2025-07-16 17:30:20 -0300

    Update Consultas_Sistema_Entrega.sql

--- DDL/Consultas_Sistema_Entrega.sql
+++ DDL/Consultas_Sistema_Entrega.sql
@@ -1,5 +1,5 @@
 use teste
--- quem comprou e o quê? e valor?
+-- quem comprou e o quÃª? e valor?
 select  c.cpf, c.nome, p.itens, p.total_pedido 
 from Cliente c inner join Pedido p on p.cpf = c.cpf;
 
@@ -17,27 +17,3 @@ inner join Entregador en on en.cnh = e.cnh;
 --Quantidade item e valor
 select pss.quantidade, p.item, p.preco
 from Possui pss inner join Produto p on pss.cod_produto = p.cod_produto;
-
-
-
--- View 
--- Pedido do cliente com avaliação, item e valor total
-create view vw_pedido_cliente_ava_entrega 
-as select c.nome, p.itens, p.total_pedido, e.avaliacao, e.status 
-from Cliente c inner join Pedido p on p.cpf = c.cpf
-inner join Entrega e on e.cod_pedido = p.cod_pedido;
-
-
-select * from vw_pedido_cliente_ava_entrega;
-
-
--- View Criptografada 
-
-create view vw_entregador_avaliacao with encryption 
-as 
-select e.nome as entregador, en.avaliacao as nota, en.status
-from Entregador e inner join Entrega en 
-on en.cnh = e.cnh;
-
-
-select * from vw_entregador_avaliacao;
